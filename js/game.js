497723e94ff09311c0c05897122d2f7e4c134071
Matthew Meinen <matthewjmeinen@gmail.com>
2025-11-06 22:23:17 -0500

    Playing with AI

--- js/game.js
+++ js/game.js
@@ -4,12 +4,9 @@ window.onresize = function(event){
 };
 
 window.onload = function(event){
-
-    ui = new UI();
     
     startGame();
 
-    ui.openWelcome(true);
 
     var canvas = document.getElementById('canvas');
     canvas.addEventListener("click", clicked);
@@ -57,8 +54,6 @@ window.onmouseup = function(event) {
     watchDrag = false;
 }
 
-var ui; 
-
 var cameraX = 0;
 var cameraY = 0;
 
@@ -75,7 +70,6 @@ var startDrag = false;
 var watchDrag = false;
 
 function startGame() {
-    ui.openWelcome(false);
     level = 0;
     nextLevel();
 }
